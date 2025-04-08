0301751b6ecd5009056e6a87404e289c4efecd9f
hooft99 <24228593+hooft99@users.noreply.github.com>
2025-04-08 19:18:37 +0200

    Updated google tag

--- resources/views/panels/analytics.blade.php
+++ resources/views/panels/analytics.blade.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GYK90YJ7XR"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZEH17CM38K"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-GYK90YJ7XR');
+  gtag('config', 'G-ZEH17CM38K');
 </script>
