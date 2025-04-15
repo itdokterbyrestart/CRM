6dedbd31eb67dc6249c9d9bb6a03e53edf2a0d16
hooft99 <24228593+hooft99@users.noreply.github.com>
2025-04-15 17:42:27 +0200

    Changed Uuid to uppercase

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\uuid;
+use App\Traits\Uuid;
 use App\Models\{
     Order,
     OrderProduct,
@@ -19,7 +19,7 @@ use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Invoice extends Model implements HasMedia, Viewable
 {
-    use uuid, HasFactory, InteractsWithMedia, InteractsWithViews;
+    use Uuid, HasFactory, InteractsWithMedia, InteractsWithViews;
 
     protected $keyType = 'uuid';
     public $incrementing = false;
--- app/traits/uuid.php
+++ app/traits/uuid.php
@@ -5,7 +5,7 @@ namespace App\Traits;
 
 use Illuminate\Support\Str;
 
-trait uuid
+trait Uuid
 {
     protected static function boot()
     {
